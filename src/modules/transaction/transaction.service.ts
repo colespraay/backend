8eb9b1d7561aeb2ff8571eccd78011f88576f0f8
Abel Ani <abelanico6@gmail.com>
2023-10-28 16:17:23 +0100

    Update

--- src/modules/transaction/transaction.service.ts
+++ src/modules/transaction/transaction.service.ts
@@ -86,7 +86,9 @@ export class TransactionService extends GenericService(TransactionRecord) {
         const item = new TransactionListHistoryGraphPartial();
         item.month = months[i - 1];
         item.monthCode = i;
-        item.totalAmount = 0;
+        // item.totalAmount = 0;
+        // TODO: remove after testing
+        item.totalAmount = 10000;
         const findItem = result.find(({ month }) => month === i);
         if (findItem) {
           item.totalAmount = findItem.incomingtotal + findItem.outgoingtotal;
