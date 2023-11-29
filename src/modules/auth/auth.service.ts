5845a2cee2557de759dcac2bb550d7c9944f2de7
Abel Ani <abelanico6@gmail.com>
2023-11-29 20:50:49 +0100

    Added email template action after login

--- src/modules/auth/auth.service.ts
+++ src/modules/auth/auth.service.ts
@@ -1,3 +1,4 @@
+import { OnEvent, EventEmitter2 } from '@nestjs/event-emitter';
 import {
   Injectable,
   HttpStatus,
@@ -12,6 +13,7 @@ import {
   validateURLField,
   checkForRequiredFields,
   compareEnumValueFields,
+  sendEmail,
 } from '@utils/index';
 import { User } from '@entities/index';
 import { sign } from 'jsonwebtoken';
@@ -27,7 +29,10 @@ import {
 export class AuthService {
   private readonly logger: Logger = new Logger(AuthService.name);
 
-  constructor(private readonly userSrv: UserService) {}
+  constructor(
+    private readonly userSrv: UserService,
+    private readonly eventEmitterSrv: EventEmitter2,
+  ) {}
 
   async login(payload: LoginUserDTO): Promise<AuthResponseDTO> {
     try {
@@ -65,6 +70,7 @@ export class AuthService {
           process.env.ENCRYPTION_KEY,
         );
         const token = this.signPayload({ data: payloadToSign });
+        this.eventEmitterSrv.emit('login.notification', id);
         return {
           success: true,
           code: HttpStatus.OK,
@@ -119,6 +125,7 @@ export class AuthService {
           process.env.ENCRYPTION_KEY,
         );
         const token = this.signPayload({ data: payloadToSign });
+        this.eventEmitterSrv.emit('login.notification', id);
         return {
           success: true,
           code: HttpStatus.OK,
@@ -260,5 +267,69 @@ export class AuthService {
 
   private signPayload<T>(payload: T): string {
     return sign(payload, process.env.JWT_SECRET, { expiresIn: '1d' });
+  }
+
+  @OnEvent('login.notification', { async: true })
+  private async sendEmailAfterLogin(userId: string): Promise<void> {
+    try {
+      const user = await this.userSrv.findUserById(userId);
+      if (user.data?.email) {
+        const instagramUrl = String(process.env.INSTAGRAM_URL);
+        const twitterUrl = String(process.env.TWITTER_URL);
+        const facebookUrl = String(process.env.FACEBOOK_URL);
+        const today = new Date();
+        const formattedDate = today.toLocaleDateString('en-US', {
+          year: 'numeric',
+          month: 'long',
+          day: 'numeric',
+        });
+        const hours = today.getHours();
+        const minutes = today.getMinutes();
+        const seconds = today.getSeconds();
+        const formattedTime = `${hours.toString().padStart(2, '0')}:${minutes
+          .toString()
+          .padStart(2, '0')}:${seconds.toString().padStart(2, '0')}`;
+
+        const html = `<section style="background: white; color: black; font-size: 15px; font-family: 'Gill Sans', 'Gill Sans MT', Calibri, 'Trebuchet MS', sans-serif; display: flex; justify-content: center; margin: 0;">
+          <div style="padding: 2rem; width: 80%;">
+              <section style="text-align: center;">
+                  <div style="text-align:center; width: fit-content; margin: 20px 0px;display: inline-block">
+                      <img src="https://ik.imagekit.io/un0omayok/Logo%20animaion.png?updatedAt=1701281040423" alt="">
+                  </div>
+              </section>
+      
+              <section style="width: 100%; height: auto; font-size: 18px; text-align: justify;">
+                  <p style="font-weight:300">Hi ${user.data.firstName},</p>
+                  <p style="font-weight:300">Please be informed that your spray app was accessed on <span style="font-weight: 400;">${formattedDate} at ${formattedTime}</span></p>
+                  <p style="font-weight:300">
+                      If you did not logon to your account at the time detailed above, please call our contact centre on
+                      <span style="font-weight: 400;"><a href="tel:070 3000000" style="color: inherit;">070 3000000,</a> <a href="tel:+234 1-2712005-7" style="color: inherit;">+234 1-2712005-7</a></span>, or send an email to
+                      <span style="font-weight: 400;"><a href="mailto:contactenter@spraay.ng" style="color: inherit;">contactenter@spraay.ng</a></span>
+                  </p>
+                  <p style="font-weight:300">Thank you for choosing spray</p>
+              </section>
+      
+              <section style="text-align: center; height: 8rem; background-color: #5B45FF; border-radius: 10px; margin-top: 2rem; margin-bottom: 2rem;">
+                <a href="${instagramUrl}" style="margin-right: 30px;display: inline-block;padding-top:40px;"><img src="https://ik.imagekit.io/un0omayok/mdi_instagram.png?updatedAt=1701281040417" alt=""></a>
+                <a href="${twitterUrl}" style="margin-right: 30px;display: inline-block;padding-top:40px;"><img src="https://ik.imagekit.io/un0omayok/simple-icons_x.png?updatedAt=1701281040408" alt=""></a>
+                <a href="${facebookUrl}" style="display: inline-block;padding-top:40px;"><img src="https://ik.imagekit.io/un0omayok/ic_baseline-facebook.png?updatedAt=1701281040525" alt=""></a>
+              </section>
+      
+              <section style="padding: 20px; border-bottom: 2px solid #000; text-align: center; font-size: 20px;">
+                  <p style="font-weight:300">Spraay software limited</p>
+              </section>
+      
+              <section style="text-align: center; font-size: 18px;">
+                  <p style="font-weight: 400;">Spraay &copy;${today.getFullYear()}</p>
+                  <p style="font-weight: 400;">Click here to <a href="#" style="color: #5B45FF;">Unsubscribe</a></p>
+              </section>
+          </div>
+      </section>`;
+        await sendEmail(html, 'Spraay app login', [user.data.email]);
+      }
+    } catch (ex) {
+      this.logger.error(ex);
+      throw ex;
+    }
   }
 }
