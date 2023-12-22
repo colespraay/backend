d1a7be52c3597eb9de4c45c887227738b340705c
Abel Ani <abelanico6@gmail.com>
2023-12-22 23:27:26 +0100

    Changed ssl settings for ormconfig

--- src/orm.config.ts
+++ src/orm.config.ts
@@ -27,7 +27,7 @@ switch (NODE_ENV) {
       // Run migrations automatically,
       // you can disable this if you prefer running migration manually.
       migrationsRun: false,
-      ssl: false,
+      ssl: { rejectUnauthorized: false },
     };
     break;
   case NODE_ENVIRONMENT.PRODUCTION:
@@ -38,7 +38,8 @@ switch (NODE_ENV) {
       dropSchema: false,
       logging: ['error', 'query'],
       logger: 'file',
-      ssl: false,
+      // ssl: false,
+      ssl: { rejectUnauthorized: false },
       cache: { duration: 20000 },
       entities: ['dist/**/*.entity.js'],
       migrations: [`${__dirname}/migrations/**/*{.ts,.js}`],
