089b18dc02b814caf07bce6f1f16ce2e32358c53
monumentale <monumentaleworks@gmail.com>
2025-03-17 16:17:00 +0100

    all changes

--- src/modules/wallet/wallet.service.ts
+++ src/modules/wallet/wallet.service.ts
@@ -117,10 +117,8 @@ export class WalletService {
     try {
       checkForRequiredFields(['userId', 'req'], payload);
       validateUUIDField(payload.userId, 'userId');
-      console.log(payload)
-      console.log(payload)
-      console.log(payload)
-      console.log(payload)
+      console.log(payload.userId)
+      console.log(payload.userId)
       const user = await this.userSrv.findUserById(payload.userId);
       const {
         data: { firstName, lastName, bvn, phoneNumber },
